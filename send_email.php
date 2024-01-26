219904ebdc223f0f34e39d904b19e54369c7fa63
mmartin46 <mmartin46@angelo.edu>
2024-01-26 09:56:53 -0600

    Fixed form handling

--- send_email.php
+++ send_email.php
@@ -8,8 +8,8 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="mail.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/mail.css">
         <style>
 
         </style>
@@ -93,7 +93,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <script src="script.js"></script>
+        <script src="js/script.js"></script>
         <script>
             $(document).ready(function () {
 
